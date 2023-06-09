863b0ed78fecdc594f5d7963ab68908cdb909dca
FranSabt <hsabatino1@gmail.com>
2023-06-09 16:35:05 -0400

    .env en DB

--- free-radius-api/src/sequeelize/database.providers.ts
+++ free-radius-api/src/sequeelize/database.providers.ts
@@ -4,6 +4,13 @@ import { UserInfo } from './user/user.entity';
 import { UserGroup } from './radusergroup/usergroup.entity';
 import { RadCheck } from './rasdusercheck/radcheck.entity';
 
+
+const HOST = process.env.DB_HOST
+const USERNAME = process.env.DB_USERNAME
+const PASSWORD = process.env.DB_PASSWORD 
+const DB = process.env.DB_NAME
+
+
 /**
  * Conexion a la BD.
  * @todo Reemplazar los valores usando por variables de entorno.
@@ -15,13 +22,13 @@ export const databaseProviders = [
       const sequelize = new Sequelize({
         dialect: 'mariadb',
         // host: '10.2.73.20',
-        host: 'localhost',
+        host: HOST,
         port: 3306,
         // username: 'fran',
-        username: 'root',
-        password: '7448280',
+        username: USERNAME,
+        password: PASSWORD,
         // database: 'radius ',
-        database: 'nest',
+        database: DB,
 
       });
       sequelize.addModels([UserInfo, UserGroup, RadCheck]);
