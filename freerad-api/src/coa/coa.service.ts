6c77c773520b97ea0bb0d401690a39bc7cf23a3b
FranSabt <hsabatino1@gmail.com>
2023-07-28 14:47:13 -0400

    coa

--- freerad-api/src/coa/coa.service.ts
+++ freerad-api/src/coa/coa.service.ts
@@ -75,7 +75,7 @@ export class CoaService {
       }
       const secret = nas.secret;
 
-      const cmd = `echo "User-Name='${username}',User-Name='${username}',NetElastic-Portal-Mode=0 | radclient -c '1' -n '3' -r '3' -t '3' -x '${ip_address}:3799' 'coa' '${secret}’ 2>&1`;
+      const cmd = `echo "User-Name='${username}',User-Name='${username}',NetElastic-Portal-Mode=0 | radclient -c '1' -n '3' -r '3' -t '3' -x '${ip_address}:3799' 'coa' '${secret}' 2>&1`;
 
       console.log(`Activando`);
       const res = await this.CoA_cmd(cmd);
